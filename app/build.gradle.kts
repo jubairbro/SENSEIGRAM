40aa3f75c2cd3e54e806e0cecb2de99b706a5483
jubairbro <jubairbro@gmail.com>
2026-02-21 05:29:33 +0600

    fix: Revert to minimal working app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
 android {
@@ -37,12 +36,4 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
-    
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
--- app/src/main/java/com/senseigram/SenseiGramApp.kt
+++ app/src/main/java/com/senseigram/SenseiGramApp.kt
@@ -1,17 +1,5 @@
 package com.senseigram
 
 import android.app.Application
-import com.senseigram.data.local.PreferenceManager
 
-class SenseiGramApp : Application() {
-    
-    companion object {
-        lateinit var prefs: PreferenceManager
-            private set
-    }
-    
-    override fun onCreate() {
-        super.onCreate()
-        prefs = PreferenceManager(this)
-    }
-}
+class SenseiGramApp : Application()
