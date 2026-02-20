771d9ecf4367481d4beb26f324c4bfa74f2ae868
jubairbro <jubairbro@gmail.com>
2026-02-21 03:06:57 +0600

    fix: Remove unused import

--- app/src/main/java/com/senseigram/ui/fragments/ComposeFragment.kt
+++ app/src/main/java/com/senseigram/ui/fragments/ComposeFragment.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
